7f4bf7018bdec579d03e77c314f73702de43c4cd
javster101 <javster101@outlook.com>
2018-06-07 23:30:51 -0400

    Throwing SQLException

--- src/main/java/org/millburn/kiosk/ServerInitializer.java
+++ src/main/java/org/millburn/kiosk/ServerInitializer.java
@@ -1,7 +1,10 @@
 package org.millburn.kiosk;
 
+import java.sql.SQLException;
+
 public class ServerInitializer {
-    public static void main(String... args){
+    public static void main(String... args) throws SQLException {
         System.out.println("Normies reee");
+        throw new SQLException();
     }
 }
